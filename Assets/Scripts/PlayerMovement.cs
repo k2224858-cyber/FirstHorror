d5c067ad6cbe7bb79027151e1b6e212377e45ceb
k2224858-cyber <k2224858@gmail.com>
2025-11-27 18:59:04 +0200

    anims + postProcessing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     void Update()
@@ -18,10 +20,16 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveInput = new Vector2(moveX, moveY).normalized;
+        SetAnim();
     }
 
     void FixedUpdate()
     {
         rb.velocity = moveInput * moveSpeed * Time.fixedDeltaTime;
+    }
+
+    private void SetAnim()
+    {
+        animator.SetBool("Walk", rb.velocity.x != 0 || rb.velocity.y != 0);
     }
 }
